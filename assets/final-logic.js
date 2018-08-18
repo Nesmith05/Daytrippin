4092d992a89aec7970d4bb59235b907745bcb487
Natalie A <nmanderson314@gmail.com>
2018-08-18 17:19:44 -0400

    hopefully resolving merge issues

--- assets/final-logic.js
+++ assets/final-logic.js
@@ -335,60 +335,62 @@ $(document).ready(function() {
                 }
             // }
         });
-    }
+    };
 
             
     //////////////////////END: NATASHA FUNCTION/////////////////////////
 
-//////////////////////MATT FUNCTION/////////////////////////
-function weather() {
-    // var destinationLat = 36.0726355 
-    // var destinationLng = -79.7919754
-    var weatherAPI = "=86e077f1801044c6bf8210536181308";
-    var queryURL = "http://api.apixu.com/v1/forecast.json?key" + weatherAPI + "&q=" + destinationLat + "," + destinationLng + "&days=3";
-    var dayonemintemp;
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    .then(function (response) {
-        console.log(queryURL);
-        console.log(response);
+    //////////////////////MATT FUNCTION/////////////////////////
+    function weather() {
+        // var destinationLat = 36.0726355 
+        // var destinationLng = -79.7919754
+        var weatherAPI = "=86e077f1801044c6bf8210536181308";
+        var queryURL = "http://api.apixu.com/v1/forecast.json?key" + weatherAPI + "&q=" + destinationLat + "," + destinationLng + "&days=3";
+        var dayonemintemp;
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
 
-        $("#date1").append("<h1>" + response.forecast.forecastday[0].date + "</h1>");
-        $("#tempmin1").append(response.forecast.forecastday[0].day.mintemp_f);
-        $("#tempmax1").append(response.forecast.forecastday[0].day.maxtemp_f);
-        $("#condition1").append(response.forecast.forecastday[0].day.condition.text);
-        $("#humidity1").append(response.forecast.forecastday[0].day.avghumidity + "%");
-        
-        $("#date2").append(response.forecast.forecastday[1].date);
-        $("#tempmin2").append(response.forecast.forecastday[1].day.mintemp_f);
-        $("#tempmax2").append(response.forecast.forecastday[1].day.maxtemp_f);
-        $("#condition2").append(response.forecast.forecastday[1].day.condition.text);
-        $("#humidity2").append(response.forecast.forecastday[1].day.avghumidity + "%");
+            $("#date1").append("<h1>" + response.forecast.forecastday[0].date + "</h1>");
+            $("#tempmin1").append(response.forecast.forecastday[0].day.mintemp_f);
+            $("#tempmax1").append(response.forecast.forecastday[0].day.maxtemp_f);
+            $("#condition1").append(response.forecast.forecastday[0].day.condition.text);
+            $("#humidity1").append(response.forecast.forecastday[0].day.avghumidity + "%");
+            
+            $("#date2").append(response.forecast.forecastday[1].date);
+            $("#tempmin2").append(response.forecast.forecastday[1].day.mintemp_f);
+            $("#tempmax2").append(response.forecast.forecastday[1].day.maxtemp_f);
+            $("#condition2").append(response.forecast.forecastday[1].day.condition.text);
+            $("#humidity2").append(response.forecast.forecastday[1].day.avghumidity + "%");
 
-        $("#date3").append(response.forecast.forecastday[2].date);
-        $("#tempmin3").append(response.forecast.forecastday[2].day.mintemp_f);
-        $("#tempmax3").append(response.forecast.forecastday[2].day.maxtemp_f);
-        $("#condition3").append(response.forecast.forecastday[2].day.condition.text);
-        $("#humidity3").append(response.forecast.forecastday[2].day.avghumidity + "%");
-        
-        $("#date1").append("");
-        reminder();
-        //Natalie - moved declaring dayonemintemp to be outside of the 'then' function and setting the variable to be within the 'then' function due to errors encountered because 'response' is not defined outside of the 'then' function
-        dayonemintemp = response.forecast.forecastday[0].day.mintemp_f;
-      });
+            $("#date3").append(response.forecast.forecastday[2].date);
+            $("#tempmin3").append(response.forecast.forecastday[2].day.mintemp_f);
+            $("#tempmax3").append(response.forecast.forecastday[2].day.maxtemp_f);
+            $("#condition3").append(response.forecast.forecastday[2].day.condition.text);
+            $("#humidity3").append(response.forecast.forecastday[2].day.avghumidity + "%");
+            
+            $("#date1").append("");
+            reminder();
+            //Natalie - moved declaring dayonemintemp to be outside of the 'then' function and setting the variable to be within the 'then' function due to errors encountered because 'response' is not defined outside of the 'then' function
+            dayonemintemp = response.forecast.forecastday[0].day.mintemp_f;
+        });
 
 
-//   var dayonemintemp = response.forecast.forecastday[0].day.mintemp_f;
-  function reminder() {
-    if (dayonemintemp < 80) {
-      $("#message1").append("Don't Forget a jacket!");
-     } else {
-      $("#message1").append("Don't forget your sunglasses!");
-     };
+    //   var dayonemintemp = response.forecast.forecastday[0].day.mintemp_f;
+    function reminder() {
+        if (dayonemintemp < 80) {
+        $("#message1").append("Don't Forget a jacket!");
+        } else {
+        $("#message1").append("Don't forget your sunglasses!");
+        };
     };
-    //////////////////////END: MATT FUNCTION/////////////////////////
+
+};
+        //////////////////////END: MATT FUNCTION/////////////////////////
 
 
     //////////////////////////////////////////////////////////////
@@ -447,49 +449,48 @@ function weather() {
         }
     });
 
-//////////////////////////////////////////////////////////////
-////////////////////////FIREBASE PULL/////////////////////////
-//////////////////////////////////////////////////////////////
-////////////// Firebase Storing User Information///////////////
-// Adding Recent searches
-$(".searchBtn").on("click", function (event) {
-    // Preventing Duplicates
-    event.preventDefault();
+    //////////////////////////////////////////////////////////////
+    ////////////////////////FIREBASE PULL/////////////////////////
+    //////////////////////////////////////////////////////////////
+    ////////////// Firebase Storing User Information///////////////
+    // Adding Recent searches
+    $(".searchBtn").on("click", function (event) {
+        // Preventing Duplicates
+        event.preventDefault();
 
 
 
-// clear text-boxes
-$("#cityInput").val("");
-$("#distanceInput").val("");
+    // clear text-boxes
+    $("#cityInput").val("");
+    $("#distanceInput").val("");
 
 
-});
+    });
 
-database.ref().limitToLast(5).on("child_added", function (childSnapshot) { console.log(childSnapshot.val());
-
-
-// assign firebase variables to snapshots.
-var fbCity = childSnapshot.val().city;
-var fbDistance = childSnapshot.val().distance;
+    database.ref().limitToLast(5).on("child_added", function (childSnapshot) { console.log(childSnapshot.val());
 
 
-// Append train info to table on page
-$("#possible-results").append("<p>" + "Within "
-+ fbDistance + " Miles of " + v.titleCase(fbCity)
-+ "</p>");
+    // assign firebase variables to snapshots.
+    var fbCity = childSnapshot.val().city;
+    var fbDistance = childSnapshot.val().distance;
 
-});
 
-//////////////////////////////////////////////////////////////
-////////////////////////END: FIREBASE PULL////////////////////
-//////////////////////////////////////////////////////////////
+    // Append train info to table on page
+    $("#possible-results").append("<p>" + "Within "
+    + fbDistance + " Miles of " + v.titleCase(fbCity)
+    + "</p>");
+    });
+
+    //////////////////////////////////////////////////////////////
+    ////////////////////////END: FIREBASE PULL////////////////////
+    //////////////////////////////////////////////////////////////
 
 
 
     //when the user selects one of the cities returned
     $(document).on("click", ".destinationCities", setDestination);
 
-//////////////////////////////////////////////////////////////
-////////////////////////END EVENTS////////////////////////////
-//////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////
+    ////////////////////////END EVENTS////////////////////////////
+    //////////////////////////////////////////////////////////////
 });
