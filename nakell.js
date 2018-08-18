429af58610b0863f515ea0eb21238b6dbb855ef6
Nakell Williams <nakellwilliams@wifi_guest-10-38-96-109.uncc.edu>
2018-08-18 12:01:52 -0400

    js api update

--- nakell.js
+++ nakell.js
@@ -2,16 +2,19 @@ function zomato(){
     var cities = {
       "async": true,
       "crossDomain": true,
-      "url": "https://developers.zomato.com/api/v2.1/cities?" + destinationLat +"&"+ destinationLng,
+      "url": "https://developers.zomato.com/api/v2.1/cities?lat=" + destinationLat +"&lon="+ destinationLng,
+      
       "method": "GET",
       "headers": {
         "user-key": "c7288644a7a1a0bb320b8e22c80479c6",
       }
+      
     }
     
     $.ajax(cities).done(function (response) {
-     // console.log(response);
+    //  console.log(response.location_suggestions[0].id);
       var cityID = response.location_suggestions[0].id;
+      // console.log(cityID);
       var restuarant = {
         "async": true,
         "crossDomain": true,
@@ -24,10 +27,10 @@ function zomato(){
       }
       
       $.ajax(restuarant).done(function (response) {
-        console.log(response.restaurants);
+        // console.log(response.restaurants);
         var list=response.restaurants;
         list.forEach(element => {
-          console.log(element.restaurant.name)
+          // console.log(element.restaurant.name)
     
           $(".table > tbody").append("<tr><td>" +  element.restaurant.name+ "</td><td>" + element.restaurant.location.address + "</td><td>"  + element.restaurant.cuisines );
         });
@@ -39,4 +42,8 @@ function zomato(){
       });
     });
     };
-        
+    
+    
+    
+    zomato();
+    
